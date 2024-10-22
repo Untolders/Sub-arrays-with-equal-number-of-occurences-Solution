3c508ff035a6339cf6d68beaa0a98287e25714e0
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2024-10-22 21:33:01 +0530

    Create Sub-arrays-with-equal-number-of-occurences-Solution.cpp

---
+++ Sub-arrays-with-equal-number-of-occurences-Solution.cpp
@@ -0,0 +1,85 @@
+############################################################################  Question  ##############################################################################################################################
+
+Given an array arr[] and two integers say, x and y, find the number of sub-arrays in which the number of occurrences of x is equal to the number of occurrences of y.
+
+Examples:
+
+Input: arr[] = [1, 2, 1] , x= 1 , y = 2
+Output: 2
+Explanation: The possible sub-arrays have same equal number of occurrences of x and y are:
+1) [1, 2], x and y have same occurrence(1).
+2) [2, 1], x and y have same occurrence(1).
+Input: arr[] = [1, 2, 1] , x = 4 , y = 6
+Output: 6
+Explanation: The possible sub-arrays have same equal number of occurrences of x and y are:
+1) [1], x and y have same occurrence(0).
+2) [2], x and y have same occurrence(0).
+3) [1], x and y have same occurrence(0).
+4) [1, 2], x and y have same occurrence(0).
+5) [2, 1], x and y have same occurrence(0).
+6) [1, 2, 1], x and y have same occurrence(0).
+Input: arr[] = [1, 2, 1] , x= 1 , y = 4
+Output: 1
+Explanation: The possible sub-array have same equal number of occurrences of x and y is: [2], x and y have same occurrence(0)
+Constraints: 
+1 <= arr.size() <= 106
+1 <= arr[i], x, y<=106
+
+
+  ##############################################################################################################  Solution  ############################################################################################
+
+  //{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    int sameOccurrence(vector<int>& arr, int x, int y) {
+        // code here
+        
+         map<int,int>mp;
+        mp[0]=1;
+        int count=0,res=0;
+        for(auto it:arr){
+            if(it==x)count++;
+            if(it==y)count--;
+            if(mp[count])res+=mp[count];
+            mp[count]+=1;
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int x = stoi(ks);
+        string ks1;
+        getline(cin, ks1);
+        int y = stoi(ks1);
+        Solution ob;
+        int ans = ob.sameOccurrence(arr, x, y);
+        cout << ans << "\n";
+        // cout<<"~"<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
